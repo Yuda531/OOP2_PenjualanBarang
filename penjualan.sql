3e7275e902ff49bf17aad1c434003271b717c280
Agung Yuda Pratama <112808394+Yuda531@users.noreply.github.com>
2024-06-19 15:19:57 +0700

    Adding Database

---
+++ penjualan.sql
@@ -0,0 +1,179 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 19, 2024 at 10:19 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_akun`
+--
+
+CREATE TABLE `tb_akun` (
+  `id_akun` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `no_tlp` int(30) NOT NULL,
+  `agama` varchar(30) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id_barang` int(11) NOT NULL,
+  `kd_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `jumlah_barang` int(11) NOT NULL,
+  `harga_beli` int(11) NOT NULL,
+  `harga_jual` int(11) NOT NULL,
+  `tgl_masuk` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`id_barang`, `kd_barang`, `nama_barang`, `jumlah_barang`, `harga_beli`, `harga_jual`, `tgl_masuk`) VALUES
+(1, 'B0001', 'Rexus Daxa Asteria AX1 V2', 1, 339000, 360000, '2024-06-19'),
+(2, 'B0002', 'Fantech Zeus X5s', 1, 150000, 175000, '2024-06-19'),
+(3, 'B0003', 'Toolkit 15 in 1', 1, 40000, 60000, '2024-06-19'),
+(4, 'B0004', 'DBE GM190', 1, 180000, 199000, '2024-06-19'),
+(5, 'B0005', 'Jakemy Toolkit', 1, 45000, 50000, '2024-06-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_hitung_jual`
+--
+
+CREATE TABLE `tb_hitung_jual` (
+  `id_hitung` int(11) NOT NULL,
+  `kd_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga_satuan` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah_jual` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Triggers `tb_hitung_jual`
+--
+DELIMITER $$
+CREATE TRIGGER `tr_batal` AFTER DELETE ON `tb_hitung_jual` FOR EACH ROW BEGIN
+UPDATE tb_barang set jumlah_barang=jumlah_barang+old.jumlah_jual
+WHERE kd_barang=old.kd_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `tr_jual` AFTER INSERT ON `tb_hitung_jual` FOR EACH ROW BEGIN
+UPDATE tb_barang set jumlah_barang=jumlah_barangnew.jumlah_jual
+WHERE kd_barang=new.kd_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_penjualan`
+--
+
+CREATE TABLE `tb_penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `no_faktur` varchar(11) NOT NULL,
+  `kd_barang` varchar(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `hsatuan` int(11) NOT NULL,
+  `jumlah_jual` int(11) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `bayar` int(11) NOT NULL,
+  `kembalian` int(11) NOT NULL,
+  `tgl_penjualan` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_akun`
+--
+ALTER TABLE `tb_akun`
+  ADD PRIMARY KEY (`id_akun`);
+
+--
+-- Indexes for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indexes for table `tb_hitung_jual`
+--
+ALTER TABLE `tb_hitung_jual`
+  ADD PRIMARY KEY (`id_hitung`);
+
+--
+-- Indexes for table `tb_penjualan`
+--
+ALTER TABLE `tb_penjualan`
+  ADD PRIMARY KEY (`id_penjualan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_akun`
+--
+ALTER TABLE `tb_akun`
+  MODIFY `id_akun` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tb_hitung_jual`
+--
+ALTER TABLE `tb_hitung_jual`
+  MODIFY `id_hitung` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tb_penjualan`
+--
+ALTER TABLE `tb_penjualan`
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
